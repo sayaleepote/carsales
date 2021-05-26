5298ff33211d430c3d0896320ccc34609bbc82c5
Sayalee Pote <sayaleepote44@gmail.com>
2021-05-26 15:48:46 +0530

    Added UI outlets from storyboard to swift files

---
+++ carsales/carsales/Views/Cells/CarsListCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CarsListCollectionViewCell.swift
+//  carsales
+//
+//  Created by Sayalee Pote on 26/05/21.
+//
+
+import UIKit
+
+class CarsListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mainCarPhoto: UIImageView!
+    @IBOutlet weak var carTitle: UILabel!
+    @IBOutlet weak var carPrice: UILabel!
+    @IBOutlet weak var carLocation: UILabel!
+    
+    
+}
---
+++ carsales/carsales/Views/ViewControllers/CarsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CarsListViewController.swift
+//  carsales
+//
+//  Created by Sayalee Pote on 26/05/21.
+//
+
+import UIKit
+
+class CarsListViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var carsListCollectionView: UICollectionView!
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
